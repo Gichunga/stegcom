6f988beebebbb779acaafa5e300f9b23cfcaa9b4
steve <gichungasteve6@gmail.com>
2021-03-11 21:35:54 +0000

    fix lodash

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -194,7 +194,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
 
           <li class="nav-item has-tree-view">
-            <a href="#" class="nav-link active">
+            <a href="#" class="nav-link">
               <i class="nav-icon fa fa-cogs"></i>
               <p>
                 Management
