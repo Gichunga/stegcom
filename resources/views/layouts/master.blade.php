f9bd11af7ccda4fe4f494d4776ba55e368e051e1
steve <gichungasteve6@gmail.com>
2021-03-11 18:36:26 +0000

    Configure Vue to laravel 8 and AdminLTE 3

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,12 +7,16 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
   <title>Stegcom | Dashboard</title>
 
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
+<div class="wrapper" id="app">
 
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand navbar-white navbar-light">
@@ -181,12 +185,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
                with font-awesome or any other icon font library -->
 
           <li class="nav-item">
-            <a href="#" class="nav-link">
+            <router-link to="/dashboard" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Dashboard
               </p>
-            </a>
+            </router-link>
           </li>
 
           <li class="nav-item has-tree-view">
@@ -212,12 +216,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
               </li>    
             </ul>
             <li class="nav-item">
-              <a href="#" class="nav-link">
+              <router-link to="/profile" class="nav-link">
                 <i class="nav-icon fas fa-user"></i>
                 <p>
                   Profile
                 </p>
-              </a>
+              </router-link>
             </li>
 
             <li class="nav-item">
@@ -260,7 +264,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Main content -->
     <div class="content">
       <div class="container-fluid">
-
+        <router-view></router-view>
       </div><!-- /.container-fluid -->
     </div>
     <!-- /.content -->
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,7 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css').js('node_modules/popper.js/dist/popper.js', 'public/js').sourceMaps();
 
 mix.scripts([
     'node_modules/jquery/dist/jquery.js',
