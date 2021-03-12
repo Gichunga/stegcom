e57c30c590dfaeec40db1910bd87b9f1c237af59
steve <gichungasteve6@gmail.com>
2021-03-12 09:44:58 +0000

    fixed the vue router fallback function and active page styling

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -185,7 +185,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                with font-awesome or any other icon font library -->
 
           <li class="nav-item">
-            <router-link to="/dashboard" class="nav-link active">
+            <router-link to="/dashboard" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Dashboard
--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,5 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-// Route::get('{path}', [App\Http\Controllers\HomeController::class])->where('path','([A-z\d\-\/_.]+)');
+Route::get('{path}', [App\Http\Controllers\HomeController::class, 'index'])->where('path','([A-z\d\-\/_.]+)');
  
