794392c87ede5908e6f425905fba5c7ae14bb381
steve <gichungasteve6@gmail.com>
2021-03-17 18:45:04 +0000

    configured a vue router for a 404 page

--- resources/js/app.js
+++ resources/js/app.js
@@ -58,7 +58,7 @@ let routes = [
     { path: '/dashboard', component: require('./components/Dashboard.vue').default},
     { path: '/profile', component: require('./components/Profile.vue').default},
     { path: '/users', component: require('./components/Users.vue').default},
-    { path: '*', component: require('./components/NotFound.vue').default},
+    { path: '*', component: require/('./components/NotFound.vue').default},
   ]
 
 const router = new VueRouter({
