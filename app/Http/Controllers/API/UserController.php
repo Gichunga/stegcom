0f71acd6e5f4800e287e91940f55feaddeadc7e5
steve <gichungasteve6@gmail.com>
2021-03-17 22:35:18 +0000

    fix bug where the server returns a 500 status code when the query receives null value

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -160,6 +160,8 @@ class UserController extends Controller
                       ->orWhere('type', 'LIKE', "%$search%");
             })->paginate(20);
 
+        }else{
+            $users = User::latest()->paginate(5);
         }
 
         return $users;
