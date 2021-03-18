0d950490bf6552a6a2c1e3f4c7b96cab19dbf25d
steve <gichungasteve6@gmail.com>
2021-03-18 00:41:42 +0000

    implement the functionality to print invoice as phdf

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,9 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::get('/invoice', function(){
+    return view('invoice');
+});
+
 Route::get('{path}', [App\Http\Controllers\HomeController::class, 'index'])->where('path','([A-z\d\-\/_.]+)');
  
